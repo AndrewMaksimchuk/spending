503b701f402753efc53f3a9818628c8ab0478b06
Andrew Maksimchuk <amaksimcthuk@gmail.com>
2026-06-04 22:21:03 +0300

    fix get_date.bash + test

--- get_date.bash
+++ get_date.bash
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
 
-projectdir=$(dirname $0)
+# Get date from receipt file
+#
+# Arguments:
+#   $1 - path to receipt file 
 
 
-. $projectdir/get_date_time.bash
+projectdir=$(dirname "$0")
+
+. "$projectdir/get_date_time.bash"
 
 
 function get_date() {
-    [[ ! -e $1 ]] && exit
+    if [[ ! -e $1 ]]; then 
+        echo "$1 this receipt file not exist"
+        exit 0
+    fi
     
-    date_time=$(get_date_time $1)
+    date_time=$(get_date_time "$1")
 
     if [[ -n $date_time ]]; then
-        echo $date_time | awk '{print $1}'
+        echo "$date_time" | awk '{print $1}'
+    else
+        echo "24.02.2022"
     fi
 }
